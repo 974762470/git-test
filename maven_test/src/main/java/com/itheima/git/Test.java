291b150afad0d98b143e0e08b276ad74c4435d28
zww <974762470@qq.com>
2020-05-05 02:08:21 +0800

    把工程提交到git

--- maven_test/src/main/java/com/itheima/git/Test.java
+++ maven_test/src/main/java/com/itheima/git/Test.java
@@ -3,5 +3,7 @@ package com.itheima.git;
 public class Test {
     public static void main(String[] args) {
         System.out.println("hello ");
+        System.out.println("hello maven");
     }
+
 }
